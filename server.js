ac8bf369f68d6db86ec493051bd1c68c1839492d
tiffanyposs <tiffanyposs@gmail.com>
2014-12-12 13:56:34 -0500

    updates

--- server.js
+++ server.js
@@ -4,23 +4,22 @@ var server = new WebSocketServer({port: 3000});
 
 var storedMessages = [];
 var storedClients = [];
-//!!!!!!!!!
-/*
-WORKS SENDING FILES TO EACHOTHER, BUT NEEDS TO ADD A FUNCTION
-THAT REMOVES EACH CLIENT FROM THE storedClients LIST OR IT WILL
-CRASH
+var counter = 1;
 
-NEED TO MAKE A COUNTER OUTSIDE OF THE SERVER ON AND ADD A KEY VALUE PAIR
-THAT WILL ASSIGN THAT NUMBER AS A VALUE, SO YOU CAN LATER REFERENCE ITS
-OWN NUMBER TO REMOVE ITSELF FROM THE storedClients LIST UPON CLOSE!
-*/
-//!!!!!!!!
 
 console.log("listening on port 3000 (press CTRL+C to quit)");
 
+
+
+
+///////
+//ON CONNECTION
+///////
 server.on("connection", function(connection){
   console.log("Client Connected");
-
+  //this logs a usernumber to each server
+  connection.usernumber = "user_" + counter;
+  counter++;
   //this keeps tract of the connections
   storedClients.push(connection);
 
@@ -28,6 +27,10 @@ server.on("connection", function(connection){
     connection.send(each);
   });
 
+
+  ///////
+  //ON MESSAGE
+  ///////
   //listen for incoming messages
   connection.on("message", function(message){
     console.log(message);
@@ -40,9 +43,30 @@ server.on("connection", function(connection){
   });//end message
 
 
+
+  ///////
+  //ON CLOSE
+  ///////
   connection.on("close", function(){
     console.log("Client Disconnected");
+    removeClient(connection);
 
   });
 
-});//end connection
+});
+/////////
+//end connection
+/////////
+
+////////
+//START FUNCTIONS
+///////
+
+// this removes the client from storedClients when they close
+var removeClient = function(connection){
+  for(var i = 0; i < storedClients.length; i++){
+    if(storedClients[i].usernumber === connection.usernumber){
+      storedClients.splice(i, 1);
+    }
+  }
+}
